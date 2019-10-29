8a650c96161ed65c085cc479c2dcb8fbde21875a
root <root@localhost.localdomain>
2019-10-29 18:19:13 +0300

    corrected task1 again

--- task1.py
+++ task1.py
@@ -4,5 +4,7 @@ x = int(input("Enter x: "))
 y = int(input("Enter y: "))
 z = int(input("Enter z: "))
 n = int(input("Enter n: "))
-print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if (
-        (i + j + k) != n)])
+print([[i, j, k]
+       for i in range(x + 1)
+       for j in range(y + 1)
+       for k in range(z + 1) if ((i + j + k) != n)])
