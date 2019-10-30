5f40344c0c0d5ab0bc72cf56d92ee73fd74ab43b
root <root@localhost.localdomain>
2019-10-30 14:11:01 +0300

    fourth

--- api_req.py
+++ api_req.py
@@ -4,31 +4,43 @@ import requests
 import argparse
 import getpass
 
-passwd = getpass.getpass()
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-user_name', nargs=1, help="write user name", required=True)
-parser.add_argument('-repo', nargs=1, help="write name of repo", required=True)
-parser.add_argument('-req', action="store_true",
-                    help="request names and last request update")
+parser.add_argument('-repo_owner', nargs=1, help="write repo owner", required=True)
+parser.add_argument('-repo_name', nargs=1, help="write name of repo", required=True)
 parser.add_argument('-basic', action="store_true",
                     help="basic account info")
+parser.add_argument('-req', action="store_true",
+                    help="request names and last request update")
 parser.add_argument('-forks', action="store_true",
                     help="forks info: owners, default branch, number of branches")
+parser.add_argument('-commits', action="store_true",
+                    help="commits_info"),
+parser.add_argument('-branches', action="store_true",
+                    help="info about branches")
 parser.add_argument('-v', action="version", version="version 0.0.7")
 args = parser.parse_args()
 
+passwd = getpass.getpass()
 url = "https://api.github.com"
 basic_info = requests.get(url + '/user', auth=(args.user_name[0], passwd))
 pulls_info = requests.get(
-    url + '/repos/' + args.user_name[0] + '/' + args.repo[0] + '/pulls',
+    url + '/repos/' + args.repo_owner[0] + '/' + args.repo_name[0] + '/pulls',
     auth=(args.user_name[0], passwd))
 stats_info = requests.get(
-    url + '/repos/' + args.user_name[0] + '/' + args.repo[0] + '/forks',
+    url + '/repos/' + args.repo_owner[0] + '/' + args.repo_name[0] + '/forks',
+    auth=(args.user_name[0], passwd))
+commits_info = requests.get(
+    url + '/repos/' + args.repo_owner[0] + '/' + args.repo_name[0] + '/commits',
+    auth=(args.user_name[0], passwd))
+branches_info = requests.get(
+    url + '/repos/' + args.repo_owner[0] + '/' + args.repo_name[0] + '/branches',
     auth=(args.user_name[0], passwd))
 data1 = basic_info.json()
 data2 = pulls_info.json()
 data3 = stats_info.json()
+data4 = commits_info.json()
+data5 = branches_info.json()
 
 if args.req:
     for i, j in enumerate(data2):
@@ -44,5 +56,12 @@ elif args.forks:
         print("Owners full name: %s" % data3[i]["full_name"])
         print("Default_branch: %s" % data3[i]["default_branch"])
         print("Number of forks: %s" % data3[i]["forks_count"])
+elif args.commits:
+    for i, j in enumerate(data4):
+        print("Author: %s - Commit date: %s" % (
+            data4[i]["commit"]["author"]["name"],
+            data4[i]["commit"]["author"]["date"]))
+elif args.branches:
+    print(data5)
 else:
     print("No arguments! Please enter the argument. For help info please enter ""-h"".")
